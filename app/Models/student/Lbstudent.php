b48e07d9216b183cb1ddd8d6b4c0802e5432cd94
rihabatool4-code <rihabatool4@gmail.com>
2026-04-24 16:19:40 -0700

    Student Authentication

--- app/Models/student/Lbstudent.php
+++ app/Models/student/Lbstudent.php
@@ -4,9 +4,21 @@ namespace App\Models\student;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Lbstudent extends Model
+class Lbstudent extends Authenticatable implements JWTSubject
 {
-    protected $fillable = ['Full_name', 'email', 'phone', 'roll_no', 'program', 'session', 'semester', 'password'];
+    protected $fillable = ['fullName', 'email', 'phone', 'roll_no', 'program', 'session', 'semester', 'password'];
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return ["guard" =>"student"];
+    }
 }
