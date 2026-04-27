06c4e0b36021ddc690c2f9add7ad925fd2379ca3
easlam525-cyber <easlam525@gmail.com>
2026-04-27 15:32:13 +0500

    student controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\student\auth\StudentAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,10 +10,14 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| routes are loaded by the RouteServiceProvider and all of them will`   
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+/////////////////***************Student Routes **************//////////////////
+
+Route::post("/student/auth/registerStudent", [StudentAuthController::class, "registerStudent"]);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
